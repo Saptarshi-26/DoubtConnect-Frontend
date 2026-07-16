fe15d0725be0a810dfdab6d974b09c7496ec5462
Saptarshi-26 <saptarshiok26@gmail.com>
2026-07-16 18:07:27 +0530

    Update frontend for production deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://doubtconnect-backend.onrender.com", // render 
+// baseURL: "https://doubtconnect-backend.onrender.com", // render 
  // baseURL: "http://localhost:8080",
    //baseURL: "https://doubtconnectbackend-nk9c1w2j.b4a.run",
-   // baseURL: "http://51.21.243.242:8080",
+    baseURL: "http://13.201.19.80:8080",
 });
 
 api.interceptors.request.use((config) => {
