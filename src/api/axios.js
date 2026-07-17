8187fde356c5781b2ea0b02fa61048628c828bac
Saptarshi-26 <saptarshiok26@gmail.com>
2026-07-17 23:24:42 +0530

    Use production backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const api = axios.create({
   
 //baseURL: "https://doubtconnect-backend.onrender.com", // render 
- baseURL: "http://localhost:8080",
+ //baseURL: "http://localhost:8080",
    //baseURL: "https://doubtconnectbackend-nk9c1w2j.b4a.run",
  //baseURL: "http://13.201.19.80:8080",
  
- //baseURL: "https://doubtconnect.duckdns.org",
+ baseURL: "https://doubtconnect.duckdns.org",
 });
 console.log("Axios file loaded");
 console.log("Base URL =", api.defaults.baseURL);
